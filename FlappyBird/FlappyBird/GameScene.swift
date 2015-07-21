0409b86977c437ca3a6fc72389e34c7834cb4aa1
Luke <luke.tehira@powershop.co.nz>
2015-07-21 17:33:37 +1200

    Demo: Step 3 - Add midground and foreground variables

--- FlappyBird/FlappyBird/GameScene.swift
+++ FlappyBird/FlappyBird/GameScene.swift
@@ -10,6 +10,13 @@ import SpriteKit
 import AVFoundation
 
 // Step 3: Layers
+enum Layer:Int {
+    case LayerBackground = 1, LayerMidground, LayerObstacle, LayerForeground, LayerPlayer, LayerFlash, LayerUI
+    
+    func floatValue() -> CGFloat {
+        return CGFloat(self.rawValue)
+    }
+}
 
 // Helper function
 func randomFloatRange(min:CGFloat, max:CGFloat) -> CGFloat {
@@ -23,11 +30,17 @@ func degreesToRadians(degrees:CGFloat) -> CGFloat {
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
     // Step 2: Create our root node for the scene graph
+    var worldNode:SKNode = SKNode()
+    
+    // Step 8: Add the playableStart variable
     
     override init(size: CGSize) {        
         super.init(size:size)
         
         // Step 5: Add the root note to the scene
+        self.addChild(worldNode)
+        
+        setupBackground()
     }
 
     required init(coder aDecoder: NSCoder) {
@@ -35,6 +48,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     // Step 4: Setup background
+    func setupBackground() {
+        let background = SKSpriteNode(imageNamed: "Background")
+        background.anchorPoint = CGPoint(x:0.5, y:1.0)
+        background.position = CGPoint(x:self.size.width / 2.0, y:self.size.height);
+        background.zPosition = Layer.LayerBackground.floatValue()
+        worldNode.addChild(background)
+        
+        // Step 9: Move the foreground to the correct position
+        
+    }
+    
+    // Step 7: Setup foreground and midground
+        
     
     override func update(currentTime: NSTimeInterval) {
         
